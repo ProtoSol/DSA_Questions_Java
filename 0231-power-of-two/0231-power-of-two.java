30c3feab491e05e9a6b0f3b63932fc50fbb92b76
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-03-02 15:35:30 +0530

    Time: 1 ms (45.65%), Space: 41 MB (20.17%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        int ans = 1;
-        for(int i = 0; i <= 30; i++){
-            if (ans == n) return true;
-            if (ans < Integer.MAX_VALUE/2) ans = ans*2;
-        }
-        return false;
+        // A number is a power of two if it has only one bit set in its binary representation.
+        // So, if we do a bitwise AND of n and (n - 1), and the result is 0, then n is a power of two.
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
