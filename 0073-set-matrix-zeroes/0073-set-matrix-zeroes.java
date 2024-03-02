dbede31fcf2482fc8a12188d65c4a2056e77644a
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-03-02 20:08:11 +0530

    Time: 1 ms (75.71%), Space: 45.6 MB (30.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,34 +5,23 @@ public class Solution {
         int n = matrix[0].length;
 
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) { // Fixed the loop indices
+            if (matrix[i][0] == 0) {
+                col0 = 0;
+            }
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    // Mark i-th row
-                    matrix[i][0] = 0;
-                    // Mark j-th column
-                    if (j != 0)
-                        matrix[0][j] = 0;
-                    else
-                        col0 = 0;
+                    matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 1; i < m; i++) { // Fixed the loop index
-            for (int j = 1; j < n; j++) { // Fixed the loop index
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
-        }
-
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        if (col0 == 0) {
-            for (int i = 0; i < m; i++) {
+            if (col0 == 0) {
                 matrix[i][0] = 0;
             }
         }
