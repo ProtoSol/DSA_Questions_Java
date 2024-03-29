f17d66997a7679bcd4979fae06460023ddecb912
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-03-29 22:42:22 +0530

    Time: 39 ms (42.53%) | Memory: 16.6 MB (10.42%) - LeetSync

---
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubstringPresent(self, s: str) -> bool:
+        N = len(s)
+        r = "".join(reversed(list(s)))
+
+        for i in range(1,N):
+            if s[i-1] + s[i] in r:
+                return True
+        return False
