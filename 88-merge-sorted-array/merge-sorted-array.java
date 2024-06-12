2da358423b1ece7f4e18b308f5c3545cb2ff6e67
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-06-12 16:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (48.19%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -5,9 +5,48 @@ class Solution {
 
         // Approach 1 merge the array, then sort it to produce the result.
 
-        for(int i = 0; i < n; i++){
-            nums1[m+i] = nums2[i];
+        // for(int i = 0; i < n; i++){
+        //     nums1[m+i] = nums2[i];
+        // }
+        // Arrays.sort(nums1);
+
+        // Approach 2
+
+        // We will use Three Pointer Approach
+        // Two pointers to read, one for writing
+
+        int[] result = new int[m + n];
+
+        int firstArrayPointer = 0;
+        int secondArrayPointer = 0;
+        int resultPointer = 0;
+
+        while (firstArrayPointer < m && secondArrayPointer < n) {
+            if (nums1[firstArrayPointer] <= nums2[secondArrayPointer]) {
+                result[resultPointer] = nums1[firstArrayPointer];
+                firstArrayPointer++;
+            } else {
+                result[resultPointer] = nums2[secondArrayPointer];
+                secondArrayPointer++;
+            }
+            resultPointer++;
         }
-        Arrays.sort(nums1);
+
+        // Copy remaining elements from nums1
+        while (firstArrayPointer < m) {
+            result[resultPointer] = nums1[firstArrayPointer];
+            firstArrayPointer++;
+            resultPointer++;
+        }
+
+        // Copy remaining elements from nums2
+        while (secondArrayPointer < n) {
+            result[resultPointer] = nums2[secondArrayPointer];
+            secondArrayPointer++;
+            resultPointer++;
+        }
+
+        // Copy merged array back to nums1
+        System.arraycopy(result, 0, nums1, 0, m + n);
     }
 }
