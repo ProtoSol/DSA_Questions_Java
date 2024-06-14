477ddd3e90759aee1d3e7d417b7932d5b9e53366
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-06-14 20:12:31 +0530

    Time: 328 ms (98.91%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,14 @@
 # Write your MySQL query statement below
+
+-- SELECT w1.id
+-- FROM Weather w1, Weather w2
+-- WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 
+-- AND w1.temperature > w2.temperature;
+
 SELECT w1.id
-FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 
-AND w1.temperature > w2.temperature;
+FROM Weather w1
+LEFT JOIN Weather w2
+ON w1.recordDate - INTERVAL 1 DAY = w2.recordDate
+WHERE w1.temperature > w2.temperature;
+
+-- This creates a problem as It thinks of dates as Number to solve we Use INTERVAL 1 Day
