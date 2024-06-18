eb0b240b64a6fd1d8e0c002c0bf7dbc57e7184c8
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-06-18 19:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (86.81%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -1,13 +1,47 @@
+// Brute Force O(n^2)
+// Space Complexity O(1)
+
+// class Solution {
+//     public int countNegatives(int[][] grid) {
+//         int count = 0;
+//         for(int row = 0; row < grid.length; row++){
+//             for(int col = 0; col < grid[row].length; col++){
+//                 if(grid[row][col] < 0){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+// Using the Sorted Hint We will be using the Binary Search
+
 class Solution {
     public int countNegatives(int[][] grid) {
         int count = 0;
         for(int row = 0; row < grid.length; row++){
-            for(int col = 0; col < grid[row].length; col++){
-                if(grid[row][col] < 0){
-                    count++;
-                }
+            int firstNegIndex = findFirstNegativeIndex(grid[row]);
+            if(firstNegIndex != -1){
+                count += grid[row].length - firstNegIndex;
             }
         }
         return count;
+    }
+
+    private int findFirstNegativeIndex(int[] nums){
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left < nums.length && nums[left] < 0 ? left : -1;
     }
 }
