c4cff2026d723e2ad15529175fd46538353b5493
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-06-19 16:59:44 +0530

    Time: 600 ms (87.95%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,7 +1,25 @@
 # Write your MySQL query statement below
 
-SELECT ROUND(AVG(IF(d.order_date = d.customer_pref_delivery_date, 1, 0)) * 100, 2) AS immediate_percentage
-FROM Delivery d
-LEFT JOIN (SELECT customer_id, MIN(order_date) as first_order_date FROM Delivery GROUP BY Customer_id) f
-ON d.customer_id = f.customer_id
-WHERE d.order_date = f.first_order_date;
+-- Beginner
+
+-- SELECT ROUND(AVG(IF(d.order_date = d.customer_pref_delivery_date, 1, 0)) * 100, 2) AS immediate_percentage
+-- FROM Delivery d
+-- LEFT JOIN (SELECT customer_id, MIN(order_date) as first_order_date FROM Delivery GROUP BY Customer_id) f
+-- ON d.customer_id = f.customer_id
+-- WHERE d.order_date = f.first_order_date;
+
+-- Intermediate
+
+-- SELECT ROUND(AVG(IF(order_date = customer_pref_delivery_date, 1, 0))*100,2) AS immediate_percentage
+-- FROM Delivery
+-- WHERE (customer_id, order_date) IN (SELECT customer_id, MIN(order_date)
+-- FROM Delivery
+-- GROUP BY customer_id);
+
+-- ADVANCED (WINDOW FUNCTION)
+-- We will be Ranking the dates and keeping the first rank
+
+SELECT ROUND(AVG(IF(order_date = customer_pref_delivery_date, 1, 0))*100,2) AS immediate_percentage
+FROM (Select *, DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY order_date ASC) AS order_rank 
+FROM Delivery) as order_ranking
+WHERE order_rank = 1;
