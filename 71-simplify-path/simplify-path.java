130d98eb34e32a419db9b8a9025e46ebdc19e26b
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-07-02 16:20:05 +0530

    Time: 4 ms (85.87%) | Memory: 43.2 MB (54.55%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -2,17 +2,23 @@ class Solution {
     public String simplifyPath(String path) {
         Stack<String> stack = new Stack<>();
         String[] directories = path.split("/");
+        StringBuilder sb = new StringBuilder();
         for (String dir : directories) {
-            if (dir.equals(".") || dir.isEmpty()) {
+            if(dir.equals(".") || dir.trim().length() == 0){
                 continue;
-            } else if (dir.equals("..")) {
-                if (!stack.isEmpty()) {
-                    stack.pop();
+            } else if(dir.equals("..")){
+                if(!stack.isEmpty()){
+                    stack.pop(); // Empty the Stack
                 }
             } else {
                 stack.push(dir);
             }
         }
-        return "/" + String.join("/", stack);
+        // Stack contains the Path and needs to be converted
+        for(String cpath: stack){
+            sb.append("/");
+            sb.append(cpath);
+        }
+        return sb.toString().length()>0?sb.toString():"/";
     }
 }
