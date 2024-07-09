7f4856c7fee8dd702d4252054067ed942a65b06e
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-07-09 16:48:28 +0530

    Time: 135 ms (12.26%) | Memory: 43.1 MB (7.70%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,14 +15,46 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        // Base Case
-        if(root == null){
-            return 0;
+        if(root == null) return 0;
+        // Iterative Method
+        // Make a Class to remeber
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(root, 1));
+        int maximumDepth = 0;
+        while(!stack.isEmpty()){
+            Pair pair = stack.pop();
+            TreeNode node = pair.node;
+            int depth = pair.depth;
+            maximumDepth = Math.max(maximumDepth, depth);
+            // Compute the left
+            if(node.left!= null){
+                stack.push(new Pair(node.left, depth + 1));
+            }
+            // Compute the Right
+            if(node.right != null){
+                stack.push(new Pair(node.right, depth + 1));
+            }
         }
-        // Move to Left
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        int max = Math.max(left, right);
-        return max + 1;
+        return maximumDepth;
+    }
+
+    // Recursive Method
+        // // Base Case
+        // if(root == null){
+        //     return 0;
+        // }
+        // // Move to Left
+        // int left = maxDepth(root.left);
+        // int right = maxDepth(root.right);
+        // int max = Math.max(left, right);
+        // return max + 1;
+}
+
+class Pair{
+    TreeNode node;
+    int depth;
+    Pair(TreeNode node, int depth){
+        this.node = node;
+        this.depth = depth;
     }
 }
