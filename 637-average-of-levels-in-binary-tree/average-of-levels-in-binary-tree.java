210d3dd56da4a402eba88e052d487ab606076fe3
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-07-13 13:21:31 +0530

    Time: 2 ms (96.65%) | Memory: 46.3 MB (5.04%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -24,17 +24,20 @@ class Solution {
             // Maintaining values
             int size = queue.size();
             double sum = 0;
-            int count = 0;
-            // Calc the nodes
+            // int count = 0;
+            // Calc the nodes and get the sum of nodes on the level
             for(int i = 0; i < size; i++){
                 TreeNode node = queue.poll();
                 sum = sum + node.val;
-                count++;
+                // count++;
+                // Traverse to next Level
                 if(node.left != null) queue.offer(node.left);
                 if(node.right!= null) queue.offer(node.right);
             }
-            result.add(sum/count);
+            // Add to the result list
+            result.add(sum/size /*replaced count with size*/);
         }
+        // Return the List
         return result;
     }
 }
