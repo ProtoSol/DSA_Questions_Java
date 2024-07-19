560c32c6829bdba78443f00162cc94324d4f03c8
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-07-19 16:47:46 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (15.98%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    private void helper(int node, int[][] isConnected, boolean visited[]){
+        // Recursive Call to connecct the neighbors.
+        visited[node] = true;
+        for(int i = 0; i < isConnected.length; i++){
+            if(isConnected[node][i] == 1 && !visited[i]){
+                // Connection is there and not visited
+                helper(i, isConnected, visited);
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int countComponents = 0;
+        boolean visited[] = new boolean[isConnected.length];
+        for(int i = 0; i < isConnected.length; i++){
+            if(!visited[i]){
+                // If not visited
+                helper(i, isConnected, visited);
+                countComponents++;
+            }
+        }
+        return countComponents;
+    }
+}
