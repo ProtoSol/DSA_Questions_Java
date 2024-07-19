2a1483f5ad86d435413feb08aba200aa18560dd1
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-07-19 17:03:32 +0530

    Time: 1 ms (91.63%) | Memory: 48.3 MB (6.42%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,12 +1,42 @@
 class Solution {
 
-    private void helper(int node, int[][] isConnected, boolean visited[]){
-        // Recursive Call to connecct the neighbors.
+    // DFS Approach
+
+    // private void helper(int node, int[][] isConnected, boolean visited[]){
+    //     // Recursive Call to connecct the neighbors.
+    //     visited[node] = true;
+    //     for(int i = 0; i < isConnected.length; i++){
+    //         if(isConnected[node][i] == 1 && !visited[i]){
+    //             // Connection is there and not visited
+    //             helper(i, isConnected, visited);
+    //         }
+    //     }
+    // }
+
+    // public int findCircleNum(int[][] isConnected) {
+    //     int countComponents = 0;
+    //     boolean visited[] = new boolean[isConnected.length];
+    //     for(int i = 0; i < isConnected.length; i++){
+    //         if(!visited[i]){
+    //             // If not visited
+    //             countComponents++;
+    //             helper(i, isConnected, visited);
+    //         }
+    //     }
+    //     return countComponents;
+    // }
+
+    private void bfsHelper(int node, int[][] isConnected, boolean visited[]){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
         visited[node] = true;
-        for(int i = 0; i < isConnected.length; i++){
-            if(isConnected[node][i] == 1 && !visited[i]){
-                // Connection is there and not visited
-                helper(i, isConnected, visited);
+        while(!queue.isEmpty()){
+            int currentNode = queue.poll();
+            for(int i = 0; i < isConnected.length; i++){
+                if(isConnected[currentNode][i] == 1 && !visited[i]){
+                    queue.offer(i);
+                    visited[i] = true;
+                }
             }
         }
     }
@@ -18,9 +48,10 @@ class Solution {
             if(!visited[i]){
                 // If not visited
                 countComponents++;
-                helper(i, isConnected, visited);
+                bfsHelper(i, isConnected, visited);
             }
         }
         return countComponents;
     }
+
 }
