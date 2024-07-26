194657ca4eec72e428db668bbac365b5315a2400
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-07-26 15:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (9.07%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,18 @@
 class Solution {
+    // Compute the Calculation and add it to the Memo
+    Map<Integer, Integer> memo = new HashMap<>();
     public int fib(int n) {
         if(n == 0 || n == 1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+        // Before Calc check if Present in memo
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        int first = fib(n-1);
+        int second = fib(n-2);
+        int third  = first + second;
+        memo.put(n,third); // Store in the Cache
+        return third;
     }
 }
