beae16f4c600c7cf740e5742f8c204d402d7a790
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-09-10 23:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.64%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,49 +1,39 @@
-class Solution {
-   public  int search(int[] nums, int target) {
-        if(nums.length<=1){
-            if(nums[0]==target){
-                return 0;
-            }else{
-                return -1;
-            }
+public class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
         }
-        int pivotElement=getPivotElement(nums);
-        int a=binarySearch(nums,target,0,pivotElement);
-        int b=binarySearch(nums,target,pivotElement+1,nums.length-1);
-        return Math.max(a,b);
-    }
-
-    public  int binarySearch(int[] arr,int target,int start,int end){
-        
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(arr[mid]==target){
-                return mid;
-            }else if(arr[mid]>target){
-                end=mid-1;
-            }else{
-                start=mid+1;
-            }
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
         }
-        return -1;
-    }
 
-    public  int getPivotElement(int[] arr){
-        int start=0,end=arr.length-1;
+        int low = 0, high = nums.length - 1;
 
-        while(start<=end){
-            int mid=start+(end-start)/2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            if(mid<arr.length-1 &&  arr[mid]>arr[mid+1]){
+            if (nums[mid] == target) {
                 return mid;
             }
-            else if(arr[mid]>=arr[start]){
-                start=mid+1;
-            }else if(arr[mid]<arr[start]){
-                end=mid-1;
+
+            // If left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // If right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
+
         return -1;
     }
-
 }
