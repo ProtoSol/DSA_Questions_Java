578fdc5ba525d7e4bb48d67e66371b20191c61ac
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-09-20 00:03:51 +0530

    Time: 9 ms (98.01%) | Memory: 47.2 MB (48.19%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,47 +1,42 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low = findMax(weights);
-        int high = sum(weights);
-        while(low <= high){
-            int mid = (low + high)/2;
-            int numberOfDays = findDays(weights, mid);
-            if(numberOfDays <= days){
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
-
-    private int findDays(int[] weights, int cap){
-        int days = 1, load = 0;
-        for(int i = 0; i < weights.length; i++){
-            if(weights[i] + load  > cap){
-                days += 1;
-                load = weights[i];
-            } else {
-                load += weights[i];
-            }
-        }
-        return days;
-    }
-
-    public static int findMax(int[] arr) {
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-    
-    public static int sum(int[] arr) {
+        // Find the sum max;
         int sum = 0;
-        for (int i : arr) {
-            sum += i;
+        int max = 0;
+        int ans = 0;
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i];
+            max = Math.max(max, weights[i]);
         }
-        return sum;
+        // If length == days return max
+        if (weights.length == days) {
+            return max;
+        }
+
+        int l = max;
+        int r = sum;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (findPackage(weights, mid, days)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean findPackage(int[] weights, int mid, int days) {
+        int d = 1;
+        int sum = 0;
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i];
+            if (sum > mid) {
+                d++;
+                sum = weights[i];
+            }
+        }
+        return d <= days;
     }
 }
