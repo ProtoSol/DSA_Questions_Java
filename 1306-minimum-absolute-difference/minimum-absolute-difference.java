a8aa1494e346c2922ab106e6c7a4e56f0efe689f
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-11-22 23:06:27 +0530

    Time: 17 ms (92.83%) | Memory: 56.9 MB (43.10%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -1,39 +1,20 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        // finding the range of the elements in the array
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int num : arr) {
-            min = Math.min(min, num);
-            max = Math.max(max, num);
-        }
-        // creating a counting array
-        int[] count = new int[max - min + 1];
-        for (int num : arr) {
-            count[num - min]++;
-        }
-        // reconstructing the sorted array from the counting array
-        List<Integer> sortedList = new ArrayList<>();
-        for (int i = 0; i < count.length; i++) {
-            while (count[i] > 0) {
-                sortedList.add(i + min);
-                count[i]--;
-            }
-        }
-        // find the minimum absolute difference
+        // sorting the array
+        Arrays.sort(arr);
+        // finding the minimum absolute difference
         int minDiff = Integer.MAX_VALUE;
-        for (int i = 1; i < sortedList.size(); i++) {
-            minDiff = Math.min(minDiff, sortedList.get(i) - sortedList.get(i - 1));
+        for (int i = 1; i < arr.length; i++) {
+            minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
         }
         // collecting all pairs with the minimum difference
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 1; i < sortedList.size(); i++) {
-            if (sortedList.get(i) - sortedList.get(i - 1) == minDiff) {
-                result.add(Arrays.asList(sortedList.get(i - 1), sortedList.get(i)));
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] == minDiff) {
+                result.add(Arrays.asList(arr[i - 1], arr[i]));
             }
         }
+        // returning the result
         return result;
     }
 }
