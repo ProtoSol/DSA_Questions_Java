eace050d2cc9b0e529f7fbacc7fe8d7e53394574
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-11-22 22:55:15 +0530

    Time: 2 ms (99.40%) | Memory: 44.6 MB (91.19%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -1,21 +1,24 @@
 class Solution {
     public int longestMountain(int[] arr) {
         int result = 0;
-
+        // two pointer approach
         for(int i = 1; i < arr.length - 1; i++){
             if(arr[i-1] < arr[i] && arr[i] > arr[i + 1]){
+                // set two pointers
                 int l = i;
                 int r = i;
+                // move the pointers
                 while(l > 0 && arr[l] > arr[l-1]){
                     l--;
                 }
                 while(r < arr.length - 1 && arr[r] > arr[r+1]){
                     r++;
                 }
+                // get the max mountain range
                 result = Math.max(result, r - l + 1);
             }
         }
-
+        // return the result
         return result;
     }
 }
