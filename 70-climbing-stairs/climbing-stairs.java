1e62ed8f5831fbdb665680bd2a7c02c4e483a75f
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-12-05 17:50:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (33.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -2,16 +2,26 @@ class Solution {
     private Map<Integer, Integer> memo = new HashMap<>();
     
     public int climbStairs(int n) {
+        // O(n) time complexity O(n) space complexity
         if(n == 0) return 0;
         if(n == 1) return 1;
         if(n == 2) return 2;
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
+        // int[] dp = new int[n+1];
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // dp[2] = 2;
+        // for(int i = 3; i < n + 1; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        int first = 1;
+        int second = 2;
         for(int i = 3; i < n + 1; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int temp = second;
+            second = first + second;
+            first = temp;
         }
-        return dp[n];
+        return second;
     }
 }
