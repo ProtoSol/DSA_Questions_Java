8dacf76a646fe653b9bdd7ef5bfe4fa668678233
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2024-12-06 18:37:22 +0530

    Time: 3 ms (71.64%) | Memory: 47.6 MB (49.10%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        
+
         int[][] memo = new int[rows][cols];
 
         for (int i = 0; i < rows; i++) {
@@ -11,28 +11,28 @@ class Solution {
             }
         }
 
-        return dfs(grid, 0, 0, memo);
+        return dfs(grid, 0, 0, memo, rows, cols);
     }
 
-    private int dfs(int[][] grid, int row, int col, int[][] memo) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-
+    private int dfs(int[][] grid, int row, int col, int[][] memo, int rows, int cols) {
+        // base case
         if (row == rows - 1 && col == cols - 1) {
-            return grid[row][col];
+            return grid[row][col]; // send back the result
         }
         if (memo[row][col] != -1) {
             return memo[row][col];
         }
-
+        // set the min sum maximum
         int minSum = Integer.MAX_VALUE;
 
+        // go to next directions
         if (row + 1 < rows) {
-            minSum = Math.min(minSum, dfs(grid, row + 1, col, memo));
+            minSum = Math.min(minSum, dfs(grid, row + 1, col, memo, rows, cols));
         }
         if (col + 1 < cols) {
-            minSum = Math.min(minSum, dfs(grid, row, col + 1, memo));
+            minSum = Math.min(minSum, dfs(grid, row, col + 1, memo, rows, cols));
         }
+        // memoise the result
         memo[row][col] = grid[row][col] + minSum;
         return memo[row][col];
     }
