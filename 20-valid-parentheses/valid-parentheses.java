d9a9c5ef5a2624930447b44e480dd3b656f490eb
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2025-02-20 14:47:36 +0530

    Time: 4 ms (14.05%) | Memory: 41.7 MB (65.34%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,31 +1,45 @@
 class Solution {
     public boolean isValid(String s) {
-        // Maintain opening closing pair
-        Map<Character, Character> pair = new HashMap<>();
-        pair.put('(', ')');
-        pair.put('{', '}');
-        pair.put('[', ']');
-        
-        // Maintain a Stack
+                // if the string is null or empty
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+
+        // Initialize the stack and the hashmap
         Stack<Character> stack = new Stack<>();
-        
-        for (char bracket : s.toCharArray()) {
-            // if opening, push in a stack
-            if (pair.containsKey(bracket)) {
-                stack.push(bracket);
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        // iterate through the string
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // if the character is an opening bracket
+            if (map.containsValue(c)) {
+                stack.push(c);
             }
-            // if closing, pop opening from the stack and compare it
+            // if the character is a closing bracket
             else {
+                // if the stack is empty
                 if (stack.isEmpty()) {
                     return false;
                 }
-                char openingBracket = stack.pop();
-                char expectedClosingBracket = pair.get(openingBracket);
-                if (bracket != expectedClosingBracket) {
+                // if the character is not the closing bracket of the opening bracket at the top
+                // of the stack
+                if (stack.peek() != map.get(c)) {
                     return false;
                 }
+                // if the character is the closing bracket of the opening bracket at the top of
+                // the stack
+                stack.pop();
             }
         }
-        return stack.isEmpty();
+
+        // if the stack is empty
+        if(!stack.isEmpty()){
+            return false;
+        }
+        return true;
     }
 }
