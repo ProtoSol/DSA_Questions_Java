e2412a395bd718ba5c37b844209c93ed12e35d57
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2025-02-20 14:48:22 +0530

    Time: 1 ms (99.61%) | Memory: 41.5 MB (96.25%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,45 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-                // if the string is null or empty
-        if (s == null || s.length() == 0) {
-            return true;
-        }
+        StringBuilder sb = new StringBuilder();
 
-        // Initialize the stack and the hashmap
-        Stack<Character> stack = new Stack<>();
-        Map<Character, Character> map = new HashMap<>();
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
-
-        // iterate through the string
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            // if the character is an opening bracket
-            if (map.containsValue(c)) {
-                stack.push(c);
-            }
-            // if the character is a closing bracket
-            else {
-                // if the stack is empty
-                if (stack.isEmpty()) {
-                    return false;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{')
+                sb.append(ch);
+            else if (ch == ')') {
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '(') {
+                    sb.deleteCharAt(sb.length() - 1);
                 }
-                // if the character is not the closing bracket of the opening bracket at the top
-                // of the stack
-                if (stack.peek() != map.get(c)) {
-                    return false;
+                else return false;
+            } else if (ch == '}') {
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '{') {
+                    sb.deleteCharAt(sb.length() - 1);
                 }
-                // if the character is the closing bracket of the opening bracket at the top of
-                // the stack
-                stack.pop();
+                else return false;
+            } else if (ch == ']') {
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '[') {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+                else return false;
             }
         }
-
-        // if the stack is empty
-        if(!stack.isEmpty()){
-            return false;
-        }
-        return true;
+        return sb.length() == 0;
     }
 }
