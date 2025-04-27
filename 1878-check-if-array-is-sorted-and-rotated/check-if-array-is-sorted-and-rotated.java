0b818b60ae6ae0a4e84cffa6048b2aaf1b861494
Pranshu Saini <85696176+ProtoSol@users.noreply.github.com>
2025-04-27 17:00:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (51.73%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -8,13 +8,15 @@ class Solution {
         if(nums[0] < nums[n - 1]){
             count++;
         }
+        // use the forloop to see there should be only one drop
         for(int i = 0; i < n - 1; i++){
             if(nums[i] > nums[i + 1]){
                 count++;
             }
+            // if detected return false
             if(count > 1) return false;
         }
-
+        // return true
         return true;
     }
 }
